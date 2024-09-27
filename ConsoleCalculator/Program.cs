16107e2ff8adabdbf4f87a67a6d6b180b30d27fc
James H. <130173167+tenxengineer@users.noreply.github.com>
2024-09-28 00:36:55 +0500

    Initial commit

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Simple Calculator");
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator (+, -, *, /):");
+            char operation = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result = 0;
+
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                        return;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error: Invalid operator.");
+                    return;
+            }
+
+            Console.WriteLine($"Result: {num1} {operation} {num2} = {result}");
+        }
+    }
+}
